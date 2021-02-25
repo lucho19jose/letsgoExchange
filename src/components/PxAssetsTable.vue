7089ae67ecf833d54bd555e6acaa7b3ed11600b3
Jose Luis <barbozagonzalesjose@gmail.com>
2021-02-25 14:59:35 -0500

    rutas dinamicas

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -14,13 +14,25 @@
       </tr>
     </thead>
     <tbody>
-      <tr class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100">
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+      <tr 
+        v-for="a in assets"
+        :key="a.id"
+        class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
+      >
+        <td>
+          <img
+            class="w-6 h-6"
+            :src="`https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`"
+            :alt="a.name"
+          />
+        </td>
+        <td># {{ a.rank }}</td>
+        <td>{{ a.name }}</td>
+        <td>{{ a.priceUsd | dollar }}</td>
+        <td>{{ a.marketCapUsd | dollar}}</td>
+        <td :class="a.changePercent24Hr.includes('-') ? 'text-red-600' : 'text-green-600'">
+          {{ a.changePercent24Hr | percent }}
+        </td>
         <td class="hidden sm:block"></td>
       </tr>
     </tbody>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-    <px-assets-table/>
+    <px-assets-table :assets="assets" />
   </div>
 </template>
 
 <script>
 import PxAssetsTable from '@/components/PxAssetsTable'
+import api from '@/api.js'
 
 export default {
   name:"Home",
-  components: { PxAssetsTable }
+  components: { PxAssetsTable },
+  data() {
+    return {
+      assets: []
+    }
+  },
+
+  created() {
+    api.getAssets().then(assets => (this.assets = assets))
+  }
 }
 </script>
